6f53ee898cef71fc394e1c7695a2387f6682874a
Iris Noh <irisnoh22@gmail.com>
2019-11-13 10:10:15 -0600

    heroku create

---
+++ build/precache-manifest.ebfd8b4cd1f0c4d61dd5e41159dd311b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c76367b7b14f2cdfbada260fc784b7fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef852c4c4d74ccaf53ff",
+    "url": "/static/css/main.c079c376.chunk.css"
+  },
+  {
+    "revision": "fc3fec954025f3ca39cc",
+    "url": "/static/js/2.f31586b4.chunk.js"
+  },
+  {
+    "revision": "ef852c4c4d74ccaf53ff",
+    "url": "/static/js/main.ce877318.chunk.js"
+  },
+  {
+    "revision": "677b53b4f990945ee06a",
+    "url": "/static/js/runtime-main.1a87a22e.js"
+  },
+  {
+    "revision": "7ce9680e738565539c06cf0326d7302f",
+    "url": "/static/media/submarine.7ce9680e.svg"
+  }
+]);
